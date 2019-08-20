552e24b6d65eab4caca2965ff3dc44f747506e19
fadafcuy <53563962+fadafcuy@users.noreply.github.com>
2019-08-20 13:22:13 +0700

    Add files via upload

---
+++ kontak_edit.blade.php
@@ -0,0 +1,46 @@
+@extends('template')
+@section('content')
+
+    <section class="main-section">
+      <div class="content">
+        <h1>Ubah Kontak</h1>
+        <hr>
+        @if($errors->any())
+        <div class="alert alert-danger">
+          <ul>
+            @foreach($errors->all() as $error)
+            <li>{{$error}}</li>
+            @endforeach
+          </ul>
+        </div>
+        @endif
+
+        @foreach($data as $datas)
+        <form action="{{ route('kontak.update', $datas->id) }}" method="post">
+          {{ csrf_field() }}
+          {{ method_field('PUT') }}
+          <div class="form-group">
+            <label for="nama">Nama:</label>
+            <input type="text" class="form-control" id="usr" name="nama" value="{{ $datas->nama}}">
+          </div>
+          <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="text" class="form-control" id="email" name="email" value="{{ $datas->email}}">
+          </div>
+          <div class="form-group">
+            <label for="nohp">No HP:</label>
+            <input type="text" class="form-control" id="nohp" name="nohp" value="{{ $datas->nohp}}">
+          </div>
+          <div class="form-group">
+            <label for="alamat">Alamat:</label>
+            <textarea type="text" class="form-control" id="alamat" name="alamat" value="{{ $datas->nohp}}"></textarea>
+          </div>
+          <div class="form-group">
+            <button type="submit" class="btn btn-md btn-primary">Submit</button>
+            <button type="reset" class="btn btn-md btn-danger">Cancel</button>
+          </div>
+        </form>
+        @endforeach
+      </div>
+    </section>
+    @endsection
